fe90aadacb57e2d8d159cdba6fd4d54edae00a3d
Simon Mika <simon@mika.se>
2019-10-29 14:55:26 +0100

    Switched to yuml diagrams.

--- block/diagram.ts
+++ block/diagram.ts
@@ -1,10 +1,11 @@
+import yuml2svg from "yuml2svg"
 import * as dom from "@typeup/dom"
 import { Renderer, addRenderer } from "../Renderer"
 
 async function render(renderer: Renderer, me: dom.block.Diagram): Promise<string> {
 	let value = me.value.trim()
-	if (value.startsWith("sequenceDiagram") || value.startsWith("gantt") || value.startsWith("graph") || value.startsWith("classDiagram") || value.startsWith("gitGraph"))
-		value = `<div class="mermaid">\n${ value }\t\t\n</div>`
+	if (value.startsWith("//"))
+		value = await yuml2svg(value)
 	return `<figure>
 		${ value }
 		<figcaption>${ await renderer.render(me.content) }</figcaption>
