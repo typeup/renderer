3890af985ec78ad972edb851a4421d8988ac080e
Simon Mika <simon@mika.se>
2019-11-14 09:07:11 +0100

    Added default values.

--- document.ts
+++ document.ts
@@ -7,18 +7,18 @@ async function render(renderer: Renderer, document: dom.Document): Promise<strin
 <html>
 	<head>
 		<meta charset="UTF-8" />
-		<title>${ renderer.getVariable("title") }</title>
-		<meta name="author" content="${ renderer.getVariable("author") }" />
+		<title>${ renderer.getVariable("title") || ""  }</title>
+		<meta name="author" content="${ renderer.getVariable("author") || ""  }" />
 		<meta name="date" content="${ renderer.getVariable("date") || new Date().toLocaleDateString() }" />
-		<meta name='identification' content="${ renderer.getVariable("identification") }"/>
-		<meta name='classification' content="${ renderer.getVariable("classification")}"/>
+		<meta name='identification' content="${ renderer.getVariable("identification") || "" }"/>
+		<meta name='classification' content="${ renderer.getVariable("classification") || "" }"/>
 		<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/default.min.css" />
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min.js"></script>
 		<script>hljs.initHighlightingOnLoad();</script>
-		<link rel="stylesheet" type="text/css" href="${ renderer.getVariable("template") + "/" + renderer.getVariable("class") + ".css" }" />
+		<link rel="stylesheet" type="text/css" href="${ renderer.getVariable("template") + "/" + renderer.getVariable("class") || "article" + ".css" }" />
 	</head>
 	<body>
-		<header><h1>${ renderer.getVariable("title") }</h1></header>
+		<header><h1>${ renderer.getVariable("title") || ""  }</h1></header>
 		${ content }
 	</body>
 </html>
