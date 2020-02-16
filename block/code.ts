887f7dd12cfc4053a1f582b78495b8ec0f89f73b
Simon Mika <simon@mika.se>
2020-02-16 17:51:30 +0100

    Removed language prefix.

--- block/code.ts
+++ block/code.ts
@@ -3,7 +3,7 @@ import { Renderer, addRenderer } from "../Renderer"
 
 async function render(renderer: Renderer, me: dom.block.Code): Promise<string> {
 	return `<figure>
-	<pre><code class="language-${me.language}">
+	<pre><code class="${me.language}">
 ${ me.value }
 	</code></pre>
 	<figcaption>${ await renderer.render(me.content) }</figcaption>
