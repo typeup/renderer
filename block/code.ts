5d50ad050de87853baaf7f3ed915131db88a4856
Simon Mika <simon@mika.se>
2020-02-16 17:48:24 +0100

    Fixed inital extra tab in code blocks.

--- block/code.ts
+++ block/code.ts
@@ -4,7 +4,7 @@ import { Renderer, addRenderer } from "../Renderer"
 async function render(renderer: Renderer, me: dom.block.Code): Promise<string> {
 	return `<figure>
 	<pre><code class="${me.language}">
-	${ me.value }
+${ me.value }
 	</code></pre>
 	<figcaption>${ await renderer.render(me.content) }</figcaption>
 </figure>
