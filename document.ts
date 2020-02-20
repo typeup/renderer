47ccc919eecb8dcaee5ddda619330175bcc8f041
Simon Mika <simon@mika.se>
2020-02-20 11:09:55 +0100

    Fixed issue with rendering date.

--- document.ts
+++ document.ts
@@ -4,12 +4,12 @@ async function render(renderer: Renderer, document: dom.Document): Promise<strin
 	const content = await renderer.render(document.content)
 	return (
 `<!doctype html>
-<html>
+<html lang="${ renderer.getVariable("language") }>
 	<head>
 		<meta charset="UTF-8" />
 		<title>${ renderer.getVariable("title") || ""  }</title>
 		<meta name="author" content="${ renderer.getVariable("author") || ""  }" />
-		<meta name="date" content="${ renderer.getVariable("date") || new Date().toLocaleDateString() }" />
+		<meta name="date" content="${ renderer.getVariable("date") || new Date().toLocaleString(undefined, { year: "numeric", month: "2-digit", day: "2-digit" }) }" />
 		<meta name='identification' content="${ renderer.getVariable("identification") || "" }"/>
 		<meta name='classification' content="${ renderer.getVariable("classification") || "" }"/>
 		<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/default.min.css" />
