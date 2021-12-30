93e1b912140a8cf0d6eacac0b8ff1ae60dd2cafe
Simon Mika <simon@mika.se>
2021-12-30 20:16:59 +0100

    Removed extra newlines.

--- block/code.ts
+++ block/code.ts
@@ -3,9 +3,7 @@ import { Renderer, addRenderer } from "../Renderer"
 
 async function render(renderer: Renderer, me: dom.block.Code): Promise<string> {
 	return `<figure>
-	<pre><code class="${me.language}">
-${ me.value.replace(/\&/gi, "&amp;").replace(/</gi, "&lt;").replace(/>/gi, "&gt;") }
-	</code></pre>
+	<pre><code class="${me.language}">${ me.value.replace(/\&/gi, "&amp;").replace(/</gi, "&lt;").replace(/>/gi, "&gt;") }</code></pre>
 	<figcaption>${ await renderer.render(me.content) }</figcaption>
 </figure>
 `
