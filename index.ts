37fb228405cfe256a23ecee06ac6754e957fa1f3
Simon Mika <simon@mika.se>
2025-12-02 18:27:37 +0100

    FIxed export.

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import { dom } from "@typeup/dom"
 import { Renderer } from "./Renderer"
-export function render(document: dom.Document): Promise<string> {
-	const renderer = Renderer.create()
-	return renderer.render(document)
+
+export namespace renderer {
+	export function render(document: dom.Document): Promise<string> {
+		const renderer = Renderer.create()
+		return renderer.render(document)
+	}
 }
 
 import "./document"
